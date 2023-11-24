2293f100182749b07c5b2a574037e7c4538bc0be
Rishi Samuel Patel <rishisamuelpatel@gmail.com>
2023-11-24 15:11:32 +1300

    Sanity

--- src/app/(studio)/studio/[[...index]]/page.tsx
+++ src/app/(studio)/studio/[[...index]]/page.tsx
@@ -1,4 +1,3 @@
-"use client";
 /**
  * This route is responsible for the built-in authoring environment using Sanity Studio.
  * All routes under your studio path is handled by this file using Next.js' catch-all routes:
@@ -8,9 +7,10 @@
  * https://github.com/sanity-io/next-sanity
  */
 
+"use client";
+
 import { NextStudio } from "next-sanity/studio";
 import config from "../../../../../sanity.config";
-import StudioHeader from "../../components/studio-header";
 
 export default function StudioPage() {
 	return (
