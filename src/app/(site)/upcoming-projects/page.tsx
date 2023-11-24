224d40b9dfc199ab34e13b03efa2ba86e9a7950d
Braedne <braedenltd@gmail.com>
2023-11-24 16:25:06 +1300

    fix

--- src/app/(site)/upcoming-projects/page.tsx
+++ src/app/(site)/upcoming-projects/page.tsx
@@ -5,15 +5,15 @@ export default async function UpcomingProjects() {
   const upcomingProjects = await getAllUpcomingProjects();
 
   return (
-		<div>
-			<div className="grid grid-cols-12 gap-x-5 text-xs px-[10px] md:px-5 w-full mt-[10px] mb-[60px]">
-				<h1 className="font-medium text-sm-3xl md:text-3xl my-24 md:mt-0 md:mb-large ">
-					Upcoming <br /> Projects
-				</h1>
-				<UpcomingProjectsList
-					projects={upcomingProjects.props.upcomingProjects}
-				/>
-			</div>
-		</div>
-	);
+    <div>
+      <div className="grid grid-cols-12 gap-x-5 text-xs px-[10px] md:px-5 w-full mt-[10px] mb-[60px]">
+        <h1 className="font-medium text-sm-3xl md:text-3xl my-24 md:mt-0 md:mb-large ">
+          Upcoming <br /> Projects
+        </h1>
+        <UpcomingProjectsList
+          projects={upcomingProjects.props.upcomingProjects}
+        />
+      </div>
+    </div>
+  );
 }
