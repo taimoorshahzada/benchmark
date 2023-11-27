6390a94cae34fea56a3f189eae2d19091203740c
Braedne <braedenltd@gmail.com>
2023-11-28 09:40:44 +1300

    fixes

--- src/app/(site)/components/footer.tsx
+++ src/app/(site)/components/footer.tsx
@@ -60,121 +60,121 @@ export default function Footer() {
   }, []);
 
   return (
-		<footer
-			className="bg-white bg-opacity-20 backdrop-blur-xl h-screen"
-			id="footer"
-		>
-			<div className="absolute bottom-0 grid grid-cols-12 gap-x-5 text-xs-medium mx-auto w-full bg-white p-5">
-				<div className="col-start-10 md:col-start-1 row-start-1 md:col-span-2">
-					<svg
-						width="80"
-						height="20"
-						viewBox="0 0 80 20"
-						fill="none"
-						xmlns="http://www.w3.org/2000/svg"
-					>
-						<path
-							fillRule="evenodd"
-							clipRule="evenodd"
-							d="M20 0L0 20V0H20ZM20 0L40 20H80L60 0H20Z"
-							fill="black"
-						/>
-					</svg>
-				</div>
-				<div className="col-start-1 col-span-9 md:col-start-10 md:col-span-3 md flex flex-col text-base md:mb-[233px]">
-					<Link
-						href="/"
-						className="hover:opacity-50 transition duration-250 w-fit"
-					>
-						Home
-					</Link>
-					<Link
-						href="/selected-projects"
-						className="hover:opacity-50 transition duration-250 w-fit"
-					>
-						Selected Projects
-					</Link>
-					<Link
-						href=""
-						className="hover:opacity-50 transition duration-250 w-fit"
-					>
-						Showhomes
-					</Link>
-					<Link
-						href=""
-						className="hover:opacity-50 transition duration-250 w-fit"
-					>
-						Upcoming Projects
-					</Link>
-					<Link
-						href=""
-						className="hover:opacity-50 transition duration-250 w-fit"
-					>
-						Our Process
-					</Link>
-					<Link
-						href=""
-						className="hover:opacity-50 transition duration-250 w-fit"
-					>
-						About Us
-					</Link>
-					<Link
-						href=""
-						className="hover:opacity-50 transition duration-250 w-fit"
-					>
-						Contact Us
-					</Link>
-				</div>
-				<div className="col-span-2 row-start-3 w-fit bg-[#F5F5F5] rounded-[5px] flex text-xs p-3 mb-[80px] cursor-pointer hover:opacity-50">
-					Login
-					<div className="ml-[50px] flex items-center">
-						<svg
-							width="7"
-							height="12"
-							viewBox="0 0 7 12"
-							fill="none"
-							xmlns="http://www.w3.org/2000/svg"
-						>
-							<path
-								d="M6.3131 5.65685L0.65625 0L0.656372 11.3138L6.3131 5.65685Z"
-								fill="black"
-							/>
-						</svg>
-					</div>
-				</div>
-				<div className="col-start-10 flex gap-x-1">
-					<SocialIcons />
-				</div>
+    <footer
+      className="bg-white bg-opacity-20 backdrop-blur-xl h-screen"
+      id="footer"
+    >
+      <div className="absolute bottom-0 grid grid-cols-12 gap-x-5 text-xs-medium mx-auto w-full bg-white p-5 footer-container">
+        <div className="col-start-10 md:col-start-1 row-start-1 md:col-span-2">
+          <svg
+            width="80"
+            height="20"
+            viewBox="0 0 80 20"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              fillRule="evenodd"
+              clipRule="evenodd"
+              d="M20 0L0 20V0H20ZM20 0L40 20H80L60 0H20Z"
+              fill="black"
+            />
+          </svg>
+        </div>
+        <div className="col-start-1 col-span-9 md:col-start-10 md:col-span-3 md flex flex-col text-base md:mb-[233px]">
+          <Link
+            href="/"
+            className="hover:opacity-50 transition duration-250 w-fit"
+          >
+            Home
+          </Link>
+          <Link
+            href="/selected-projects"
+            className="hover:opacity-50 transition duration-250 w-fit"
+          >
+            Selected Projects
+          </Link>
+          <Link
+            href=""
+            className="hover:opacity-50 transition duration-250 w-fit"
+          >
+            Showhomes
+          </Link>
+          <Link
+            href=""
+            className="hover:opacity-50 transition duration-250 w-fit"
+          >
+            Upcoming Projects
+          </Link>
+          <Link
+            href=""
+            className="hover:opacity-50 transition duration-250 w-fit"
+          >
+            Our Process
+          </Link>
+          <Link
+            href=""
+            className="hover:opacity-50 transition duration-250 w-fit"
+          >
+            About Us
+          </Link>
+          <Link
+            href=""
+            className="hover:opacity-50 transition duration-250 w-fit"
+          >
+            Contact Us
+          </Link>
+        </div>
+        <div className="col-span-2 row-start-3 w-fit bg-[#F5F5F5] rounded-[5px] flex text-xs p-3 mb-[80px] cursor-pointer hover:opacity-50 flex items-center h-[50px]">
+          Login
+          <div className="ml-[50px] flex items-center">
+            <svg
+              width="7"
+              height="12"
+              viewBox="0 0 7 12"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M6.3131 5.65685L0.65625 0L0.656372 11.3138L6.3131 5.65685Z"
+                fill="black"
+              />
+            </svg>
+          </div>
+        </div>
+        <div className="col-start-10 flex gap-x-1">
+          <SocialIcons />
+        </div>
 
-				<div className="row-start-4 col-span-12 grid grid-cols-12">
-					<div className="col-span-12 md:col-span-3 flex flex-col text-xs-medium">
-						<span>
-							<Link
-								href="tel:033438260"
-								className="hover:opacity-50 transition duration-250 w-fit"
-							>
-								+64 3 343 8260
-							</Link>
-							<br />
-							<Link
-								href="mailto:info@benchmarkhomes.co.nz"
-								className="hover:opacity-50 transition duration-250 w-fit"
-							>
-								info@benchmarkhomes.co.nz
-							</Link>
-						</span>
-					</div>
-					<div className="col-span-12 md:col-start-4 md:col-span-3 text-xs-medium">
-						12 Whitburn Ave, Milns Park,
-						<br /> Halswell, Christchurch, New Zealand
-					</div>
-					<div className="col-span-12 md:col-start-10 md:col-span-3 text-xxs font-medium">
-						Copyright © 2023 Benchmark Homes | All Rights Reserved. View{" "}
-						<span className="underline">Privacy Policy</span> &{" "}
-						<span className="underline">Email Disclaimer.</span>
-					</div>
-				</div>
-			</div>
-		</footer>
-	);
+        <div className="row-start-4 col-span-12 grid grid-cols-12">
+          <div className="col-span-12 md:col-span-3 flex flex-col text-xs-medium">
+            <span>
+              <Link
+                href="tel:033438260"
+                className="hover:opacity-50 transition duration-250 w-fit"
+              >
+                +64 3 343 8260
+              </Link>
+              <br />
+              <Link
+                href="mailto:info@benchmarkhomes.co.nz"
+                className="hover:opacity-50 transition duration-250 w-fit"
+              >
+                info@benchmarkhomes.co.nz
+              </Link>
+            </span>
+          </div>
+          <div className="col-span-12 md:col-start-4 md:col-span-3 text-xs-medium">
+            12 Whitburn Ave, Milns Park,
+            <br /> Halswell, Christchurch, New Zealand
+          </div>
+          <div className="col-span-12 md:col-start-10 md:col-span-3 text-xxs font-medium">
+            Copyright © 2023 Benchmark Homes | All Rights Reserved. View{" "}
+            <span className="underline">Privacy Policy</span> &{" "}
+            <span className="underline">Email Disclaimer.</span>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
 }
--- src/app/(site)/components/processes_list.tsx
+++ src/app/(site)/components/processes_list.tsx
@@ -69,99 +69,98 @@ export default function ProcessesList({ processes, walkthroughs }: any) {
   }, [processes]);
 
   return (
-		<div className="md:flex relative">
-			<div className="md:w-1/2 bg-black relative">
-				<div className="fixed md:sticky top-0 h-[115px] md:h-screen bg-black w-full">
-					<div className="h-full flex flex-col justify-end pl-3 pb-5 md:pb-10">
-						
-						<h1
-							className="font-medium text-white text-sm md:text-xl md:mb-medium"
-							id="process-title-index"
-						>
-							01
-						</h1>
-						<h1
-							className="font-medium text-white text-sm md:text-xl text-wrap"
-							id="process-title"
-						>
-							{processes ? processes[0].title : ""}
-						</h1>
-					</div>
-				</div>
-			</div>
-			<div className="md:w-1/2">
-				<h1 className="hidden md:block font-medium text-sm-xl md:text-xl mb-[90px] px-[10px] md:px-5">
-					Our <br /> Process
-				</h1>
-				{processes.map((process: any, index: number) => (
-					<div
-						key={index}
-						className={`w-full process ${
-							index == processes.length - 1 ? "" : "px-[10px] md:px-5"
-						}`}
-					>
-						{index == processes.length - 1 ? <div className=""></div> : ""}
-						<div
-							ref={index === processes.length - 1 ? sectionRef : null}
-							className={`${
-								index === processes.length - 1
-									? `mt-[50px] pt-[400px] px-[10px] md:px-5 fade-in-section ${
-											isInView ? "bg-black text-white" : ""
-									  }`
-									: "mt-large border-grey border-dashed border-t border-1"
-							} flex flex-col`}
-						>
-							{index == processes.length - 1 ? (
-								""
-							) : (
-								<div className="mt-[10px] mb-10 text-xs-medium">
-									{index < 10 ? `0${index + 1}` : index}
-								</div>
-							)}
-							<Image
-								className="w-full h-auto mb-[10px] aspect-[4/3] object-cover"
-								src={process.hero_image.imageUrl}
-								alt="Process Image"
-								width={1000}
-								height={1000}
-							/>
-							<div className="text-xs whitespace-pre-line">
-								{process.description}
-							</div>
-						</div>
-					</div>
-				))}
-				<div className="pt-large grid grid-cols-2 gap-x-[2px] text-xs—medium pb-[180px] bg-black text-white">
-					<div className="mb-5 px-5 font-medium">Virtual Walkthroughs</div>
-					{walkthroughs.props.walkthroughs.map(
-						(walkthrough: any, index: number) => (
-							<div
-								key={index}
-								className={`${
-									index % 2 == 0
-										? "col-start-1 ml-5 pr-[9px] mb-14"
-										: "col-start-2 pl-[9px] mr-5"
-								} flex flex-col border-grey border-dashed border-t border-1`}
-							>
-								<div className="mt-[10px] font-medium">{walkthrough.title}</div>
-								<div className="mb-4">{walkthrough.location}</div>
-								<iframe
-									allowFullScreen
-									src={walkthrough.url}
-									title={walkthrough.title}
-								></iframe>
-							</div>
-						)
-					)}
-					{walkthroughs.props.walkthroughs.length % 2 != 0 ? (
-						<div className="border-grey border-dashed border-t border-1 mr-5"></div>
-					) : (
-						""
-					)}
-				</div>
-				<div></div>
-				<div></div>
-			</div>
-		</div>
-	);
+    <div className="md:flex relative ">
+      <div className="md:w-1/2 bg-black relative pb-[200px]">
+        <div className="fixed md:sticky top-0 h-[115px] md:h-screen bg-black w-full">
+          <div className="h-full flex flex-col justify-end pl-3 pb-5 md:pb-10">
+            <h1
+              className="font-medium text-white text-sm md:text-xl md:mb-medium"
+              id="process-title-index"
+            >
+              01
+            </h1>
+            <h1
+              className="font-medium text-white text-sm md:text-xl text-wrap"
+              id="process-title"
+            >
+              {processes ? processes[0].title : ""}
+            </h1>
+          </div>
+        </div>
+      </div>
+      <div className="md:w-1/2">
+        <h1 className="hidden md:block font-medium text-sm-xl md:text-xl mb-[90px] px-[10px] md:px-5 mt-0 md:mt-6">
+          Our <br /> Process
+        </h1>
+        {processes.map((process: any, index: number) => (
+          <div
+            key={index}
+            className={`w-full process ${
+              index == processes.length - 1 ? "" : "px-[10px] md:px-5"
+            }`}
+          >
+            {index == processes.length - 1 ? <div className=""></div> : ""}
+            <div
+              ref={index === processes.length - 1 ? sectionRef : null}
+              className={`${
+                index === processes.length - 1
+                  ? `mt-[50px] pt-[400px] px-[10px] md:px-5 fade-in-section ${
+                      isInView ? "bg-black text-white" : ""
+                    }`
+                  : "mt-large border-grey border-dashed border-t border-1"
+              } flex flex-col`}
+            >
+              {index == processes.length - 1 ? (
+                ""
+              ) : (
+                <div className="mt-[10px] mb-10 text-xs-medium">
+                  {index < 10 ? `0${index + 1}` : index}
+                </div>
+              )}
+              <Image
+                className="w-full h-auto mb-[10px] aspect-[4/3] object-cover"
+                src={process.hero_image.imageUrl}
+                alt="Process Image"
+                width={1000}
+                height={1000}
+              />
+              <div className="text-xs whitespace-pre-line">
+                {process.description}
+              </div>
+            </div>
+          </div>
+        ))}
+        <div className="pt-large grid md:grid-cols-2 gap-x-[2px] text-xs—medium pb-[180px] bg-black text-white px-5 md:px-0">
+          <div className="mb-5 md:px-5 font-medium">Virtual Walkthroughs</div>
+          {walkthroughs.props.walkthroughs.map(
+            (walkthrough: any, index: number) => (
+              <div
+                key={index}
+                className={`${
+                  index % 2 == 0
+                    ? "col-start-1 md:ml-5 pr-[9px]"
+                    : "md:col-start-2 pl-[9px] md:mr-5"
+                } mb-14 flex flex-col border-grey border-dashed border-t border-1`}
+              >
+                <div className="mt-[10px] font-medium">{walkthrough.title}</div>
+                <div className="mb-4">{walkthrough.location}</div>
+                <iframe
+                  allowFullScreen
+                  src={walkthrough.url}
+                  title={walkthrough.title}
+                ></iframe>
+              </div>
+            )
+          )}
+          {walkthroughs.props.walkthroughs.length % 2 != 0 ? (
+            <div className="border-grey border-dashed border-t border-1 mr-5"></div>
+          ) : (
+            ""
+          )}
+        </div>
+        <div></div>
+        <div></div>
+      </div>
+    </div>
+  );
 }
