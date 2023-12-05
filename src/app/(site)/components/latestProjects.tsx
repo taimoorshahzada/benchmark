6f1716b19a7c5f826c1baab14394fd64152ae3d6
Rishi Samuel Patel <rishisamuelpatel@gmail.com>
2023-12-06 12:09:35 +1300

    fixed homepage scroller

--- src/app/(site)/components/latestProjects.tsx
+++ src/app/(site)/components/latestProjects.tsx
@@ -4,19 +4,54 @@ import { ScrollTrigger } from "gsap/dist/ScrollTrigger";
 import gsap from "gsap";
 
 function LatestProjects() {
-  return (
-    <section className="scroll-section-outer overflow-x-hidden col-span-12 pb-10 pt-6">
-      <p className="font-medium text-sm mb-5 col-span-12 pl-[10px] md:pl-5">
-        Latest Projects
-      </p>
-      <div className="swipe-section w-[400vw] flex flex-row relative md:h-[90vh]">
-        <Project />
-        <Project />
-        <Project />
-        <Project />
-      </div>
-    </section>
-  );
+	const sectionRef = useRef(null);
+	const triggerRef = useRef(null);
+
+	gsap.registerPlugin(ScrollTrigger);
+
+	useEffect(() => {
+		const pin = gsap.fromTo(
+			sectionRef.current,
+			{
+				translateX: 0,
+			},
+			{
+				translateX: "-300vw",
+				ease: "none",
+				duration: 1,
+				scrollTrigger: {
+					trigger: triggerRef.current,
+					start: "top top",
+					end: "2000 top",
+					scrub: 0.6,
+					pin: true,
+				},
+			}
+		);
+
+		return () => {
+			pin.kill();
+		};
+	}, []);
+
+	return (
+		<section className="scroll-section-outer overflow-x-hidden col-span-12 pb-10 pt-6">
+			<div ref={triggerRef}>
+				<p className="font-medium text-sm my-5 col-span-12 pl-[10px] md:pl-5">
+					Latest Projects
+				</p>
+				<div
+					ref={sectionRef}
+					className="scroll-section-inner w-[400vw] flex flex-row relative md:h-[90vh]"
+				>
+					<Project />
+					<Project />
+					<Project />
+					<Project />
+				</div>
+			</div>
+		</section>
+	);
 }
 
 export default LatestProjects;
--- src/app/(site)/page.tsx
+++ src/app/(site)/page.tsx
@@ -10,93 +10,11 @@ import Mortimer from "./assets/images/Projects-Mortimer-1500-x-1000-High-Res-2.j
 import Container from "./components/container";
 import Project from "./components/project";
 import AboutSection from "./components/about-us";
+import LatestProjects from "./components/latestProjects";
 
 gsap.registerPlugin(ScrollTrigger);
 export default function Home() {
-  useEffect(() => {
-    let currentIndex = -1;
-    let animating: any;
-    let swipePanels = gsap.utils.toArray(".swipe-section .panel");
-    gsap.set(".x-100", {
-      xPercent: 100,
-    });
-    gsap.set(swipePanels, {
-      zIndex: (i) => i,
-    });
-
-    let intentObserver = ScrollTrigger.observe({
-      type: "wheel,touch",
-      onUp: () => !animating && gotoPanel(currentIndex + 1, true),
-      onDown: () => !animating && gotoPanel(currentIndex - 1, false),
-      wheelSpeed: -1,
-      tolerance: 10,
-      preventDefault: true,
-      onPress: (self) => {
-        ScrollTrigger.isTouch && self.event.preventDefault();
-      },
-    });
-    intentObserver.disable();
-    let preventScroll = ScrollTrigger.observe({
-      preventDefault: true,
-      type: "wheel,scroll",
-      allowClicks: true,
-      onEnable: (self) => ((self as any).savedScroll = self.scrollY()),
-      onChangeY: (self) => self.scrollY((self as any).savedScroll),
-    });
-    preventScroll.disable();
-    function gotoPanel(index: any, isScrollingDown: any) {
-      animating = true;
-      if (
-        (index === swipePanels.length && isScrollingDown) ||
-        (index === -1 && !isScrollingDown)
-      ) {
-        intentObserver.disable();
-        preventScroll.disable();
-        animating = false;
-        preventScroll.scrollY(
-          preventScroll.scrollY() + (index === swipePanels.length ? 1 : -1)
-        );
-        return;
-      }
-      let target: any = isScrollingDown
-        ? swipePanels[index]
-        : swipePanels[currentIndex];
-      gsap.to(target, {
-        xPercent: isScrollingDown ? 0 : 100,
-        duration: 0.75,
-        onComplete: () => {
-          animating = false;
-        },
-      });
-      currentIndex = index;
-    }
-    ScrollTrigger.create({
-      trigger: ".swipe-section",
-      pin: true,
-      anticipatePin: 0,
-      start: "top top",
-      end: "+-100vh",
-      pinSpacing: false,
-
-      onEnter: (self) => {
-        if (preventScroll.isEnabled === false) {
-          self.scroll(self.start);
-          preventScroll.enable();
-          intentObserver.enable();
-          gotoPanel(currentIndex + 1, true);
-        }
-      },
-      onEnterBack: (self) => {
-        if (preventScroll.isEnabled === false) {
-          self.scroll(self.start);
-          preventScroll.enable();
-          intentObserver.enable();
-          gotoPanel(currentIndex - 1, false);
-        }
-      },
-    });
-  }, []);
-  return (
+	return (
 		<>
 			<Container>
 				<h1 className="font-medium text-sm-3xl md:text-3xl col-span-12 md:col-span-11 m-translate-x-[10px] md:-translate-x-5 mb-large mt-24 md:mt-3">
@@ -133,20 +51,7 @@ export default function Home() {
 				</p>
 			</Container>
 
-			<div className="swipe-section flex flex-col justify-center md:block ">
-				<div className="absolute top-[20%] md:top-0 md:relative font-medium text-sm mt-5 py-5 col-span-12 pl-[10px] md:pl-5">
-					Latest Projects
-				</div>
-				<section className="panel bg-white">
-					<Project />
-				</section>
-				<section className="panel x-100 bg-white">
-					<Project />
-				</section>
-				<section className="panel x-100 bg-white">
-					<Project />
-				</section>
-			</div>
+			<LatestProjects />
 
 			<AboutSection />
 		</>
